8e99f91f7cbbfc223a0b480ab8a2579116c516d2
Rodolfo Higuera <43897359+OFO323@users.noreply.github.com>
2020-11-28 17:09:16 -0800

    Update Home.js

--- client/src/Attempts/1st/Components/Home.js
+++ client/src/Attempts/1st/Components/Home.js
@@ -10,6 +10,10 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
+//import ListGroup from 'react-bootstrap/ListGroup';
+import Accordion from 'react-bootstrap/Accordion';
+import Badge from 'react-bootstrap/Badge';
+import Table from 'react-bootstrap/Table';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -17,94 +21,157 @@ class Home extends Component{
     render(){
         return(
             <Fragment>
+                <Container fluid>
+                    <Row>
+                        <h1><strong>Bay Area Rapid Transit</strong></h1>
+                    </Row>
+                    <Row>
+                        <h4><small>Capital Project Managment Database</small></h4>
+                    </Row>
+                </Container>
+                <Navbar expand = 'lg' sticky = 'top' bg = 'dark' variant = 'dark'>
+                    <Navbar.Brand>
+                        <img src = './train.png' alt = "BART" width = '30' height = '30'></img>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav"></Navbar.Collapse>
+                    <Nav className = 'mr-auto'>
+                        <Nav.Link >
+                            <Link to = {'/'} className = 'nav-link'>HOME</Link>
+                        </Nav.Link>
+                        <Nav.Link >
+                            <Link to = {'/assets'} className = 'nav-link'>Assets</Link>
+                        </Nav.Link>
+                        <Nav.Link >
+                            <Link to = {'/workorders'} className = 'nav-link'>Workorders</Link>
+                        </Nav.Link>
+                        <Nav.Link >
+                            <Link to = {'/createWorkorders'} className = 'nav-link'>Create Workorder</Link>
+                        </Nav.Link>
+                        <NavDropdown title = 'Search Database'>
+                            <NavDropdown.Item href = 'assetSearch'>Assets</NavDropdown.Item>
+                            <NavDropdown.Item href = 'WOSearch'>WorkOrders</NavDropdown.Item>
+                            <NavDropdown.Item href = 'projectsSearch'>Projects</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar>
 
-            <Container fluid>
-                <Row>
-                    <h1>Bay Area Rapid Transit</h1>
-                </Row>
-                <Row>
-                <h4><small>Capital Project Managment Database</small></h4>
-                </Row>
-            </Container>
-            <Navbar expand = 'lg' sticky = 'top' bg = 'dark' variant = 'dark'>
-                <Navbar.Brand>
-                    <img src = './train.png' alt = "BART" width = '30' height = '30'></img>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav"></Navbar.Collapse>
-                <Nav className = 'mr-auto'>
-                    <Nav.Link >
-                        <Link to = {'/'} className = 'nav-link'>HOME</Link>
-                    </Nav.Link>
-                    <Nav.Link >
-                        <Link to = {'/assets'} className = 'nav-link'>Assets</Link>
-                    </Nav.Link>
-                    <Nav.Link >
-                        <Link to = {'/workorders'} className = 'nav-link'>Workorders</Link>
-                    </Nav.Link>
-                    <Nav.Link >
-                        <Link to = {'/createWorkorders'} className = 'nav-link'>Create Workorder</Link>
-                    </Nav.Link>
-                    <NavDropdown title = 'Search Database'>
-                        <NavDropdown.Item href = 'assetSearch'>Assets</NavDropdown.Item>
-                        <NavDropdown.Item href = 'WOSearch'>WorkOrders</NavDropdown.Item>
-                        <NavDropdown.Item href = 'projectsSearch'>Projects</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-            </Navbar>
-
-            <Card style= {{textAlign : "left"}}>
-                <h1 style = {{bg : 'lightgrey'}}>Notifications</h1>
-            </Card>
-
-        <div className = "list-group">
-            <a href = "#alertPage" className = "list-group-item list-group-item-danger">Alert/Danger<span className = "badge">2</span></a>
-            <a href = "#infoPage" className = "list-group-item list-group-item-info">Info<span className = "badge">5</span></a>
-            <a href = "#successPage" className = "list-group-item list-group-item-success">Success<span className = "badge">3</span></a>
-            <a href = "#progPage" className = "list-group-item list-group-item-warning">Progess<span className = "badge">4</span></a>
-        </div>
-        <div className = "container-fluid">
-            <div className = "page-header">
-                <h3>Recent Updates</h3>
-            </div>
-        </div>
-        <table className="table table-striped table-dark">
-            <thead>
-                <tr>
-                <th scope="col-lg-5">Workorders</th>
-                <th scope="col-lg-5">Involved Asset</th>
-                <th scope="col-lg-2">PM/CM</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>615648</td>
-                    <td>AC-23</td>
-                    <td>PM</td>
-                </tr>
-                <tr>
-                    <td>614568 [UPDATED]</td>
-                    <td>TC-65</td>
-                    <td>CM</td>
-                </tr>
-                <tr>
-                    <td>614568</td>
-                    <td>TC-65</td>
-                    <td>CM</td>
-                </tr>
-                <tr>
-                    <td>615646</td>
-                    <td>MB-13</td>
-                    <td>PM</td>
-                </tr>
-                <tr>
-                    <td>615876</td>
-                    <td>AD-47</td>
-                    <td>CM</td>
-                </tr>
-            </tbody>
-        </table>
-
+                <Card style= {{textAlign : "left", bg : 'lightgrey'}}>
+                    <h2><u>Notifications</u></h2>
+                </Card>
+                <Accordion>  
+                        <Accordion.Toggle as = {Card.Header} eventKey = "0">
+                            Alert <Badge variant = 'danger'>Value</Badge>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey = '0'>
+                            <Card.Body>
+                            <Table striped bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th>Asset #</th>
+                                        <th>Project</th>
+                                        <th>Description</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>615648</td>
+                                        <td>AC-23</td>
+                                        <td>PM</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                        <Accordion.Toggle as = {Card.Header} eventKey = "1">
+                            Info<Badge variant = 'info'>Value</Badge>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey = '1'>
+                            <Card.Body>
+                            <Table striped bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th>Asset #</th>
+                                        <th>Project</th>
+                                        <th>Description</th>
+                                    </tr>
+                                </thead>
+                            </Table>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                        <Accordion.Toggle as = {Card.Header} eventKey = "2">
+                            Success<Badge variant = 'success'>Value</Badge>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey = '2'>
+                            <Card.Body>
+                            <Table striped bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th>Asset #</th>
+                                        <th>Project</th>
+                                        <th>Description</th>
+                                    </tr>
+                                </thead>
+                            </Table>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                        <Accordion.Toggle as = {Card.Header} eventKey = "3">
+                            Progress<Badge variant = 'primary'>Value</Badge>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey = '3'>
+                            <Card.Body>
+                            <Table striped bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th>Asset #</th>
+                                        <th>Project</th>
+                                        <th>Description</th>
+                                    </tr>
+                                </thead>
+                            </Table>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                </Accordion>
+                <Card  style = {{textAlign : 'left'}}>
+                    <h2><u>Recent Updates</u></h2>
+                </Card>
+                <table className="table table-striped table-dark">
+                    <thead>
+                        <tr>
+                        <th scope="col-lg-5">Workorders</th>
+                        <th scope="col-lg-5">Involved Asset</th>
+                        <th scope="col-lg-2">PM/CM</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>615648</td>
+                            <td>AC-23</td>
+                            <td>PM</td>
+                        </tr>
+                        <tr>
+                            <td>614568 [UPDATED]</td>
+                            <td>TC-65</td>
+                            <td>CM</td>
+                        </tr>
+                        <tr>
+                            <td>614568</td>
+                            <td>TC-65</td>
+                            <td>CM</td>
+                        </tr>
+                        <tr>
+                            <td>615646</td>
+                            <td>MB-13</td>
+                            <td>PM</td>
+                        </tr>
+                        <tr>
+                            <td>615876</td>
+                            <td>AD-47</td>
+                            <td>CM</td>
+                        </tr>
+                    </tbody>
+                </table>
+            
         </Fragment>
         )
     }
