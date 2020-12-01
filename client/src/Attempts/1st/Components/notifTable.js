a8ca7e55989edf3ffa65768e229cc0e3b03b32d5
Rodolfo Higuera <43897359+OFO323@users.noreply.github.com>
2020-11-30 23:38:17 -0800

    Update notifTable.js
    started connecting to DB contents but still incomplete

--- client/src/Attempts/1st/Components/notifTable.js
+++ client/src/Attempts/1st/Components/notifTable.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 //import {Link} from "react-router-dom";
 import '../styles.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import moment from 'moment'
+
 import Table from 'react-bootstrap/Table';
 
 
@@ -10,12 +10,10 @@ import Table from 'react-bootstrap/Table';
 
 class notifTable extends Component{
     
-   constructor(props){
-       super(props);
-       this.state = {          //specifiy notification type [danger, info, etc]
+    state = {
+        type : '',          //specifiy notification type [danger, info, etc]
         projectUpdates : []
     }
-   }
 
     //once compnent mounts data is pulled from DB
     componentDidMount(){
@@ -23,14 +21,10 @@ class notifTable extends Component{
     }
 
     //should just fetch from DB
-    getUpdates = () => {
-
-        var date = moment().utcOffset('0').format('YYYY-MM-DD');
-        console.log(date)
-
-        fetch(`http://localhost:4006/projUpdates/${date}`)
+    getUpdates = _ => {
+        fetch('localhost:4006/projUpdates')
             .then(response => response.json())
-            .then(response => this.setState({projectUpdates:response}))
+            .then(response => this.setState({projectUpdates : response.data }))
             .catch(err => console.log(err))
     } 
 
@@ -38,27 +32,35 @@ class notifTable extends Component{
 
     render(){
         const {projectUpdates} = this.state;
-        console.log(this.state)
 
         return(
             <Table striped bordered hover>
                 <thead>
                     <tr>
-                        <th></th>
+                        <th>Asset</th>
                         <th>Project</th>
                         <th>Description</th>
                     </tr>
-                    {projectUpdates.map ((item)=>{
-                            return(
-                                <tr>
-                                    <li>{item.assetID}</li>
-                                    <li>{item.projID}</li>
-                                    <li>{item.progress}</li>
-                                    <li>{item.updated_at}</li>
-                                </tr>
-                            )
-                        })}
                 </thead>
+                <tbody>
+                    {projectUpdates.length ? (
+                        <div>
+                            {projectUpdates.map((item) => {
+                                return (
+                                    <div>
+                                        <th className = "list-inline" ></th>
+                                            <td class="list-inline-item"> {}</td>
+                                            <td class="list-inline-item"> {props.dept} </td>
+                                            <td class="list-inline-item"> {props.meterName} </td>
+                                            <td class="list-inline-item"> {props.reading} </td>
+                                            <td class="list-inline-item"> {props.date} </td>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )
+                    }
+                </tbody>
             </Table>
         )
 
