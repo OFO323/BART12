0b796d90593cdf2c4b0958cddab6e63f3c1ba726
nothingisit4me <slee435@ucmerced.edu>
2020-12-04 15:38:05 -0800

    added navbar

--- client/src/Attempts/1st/Components/GraphContainer.js
+++ client/src/Attempts/1st/Components/GraphContainer.js
@@ -1,10 +1,13 @@
 import React, { Component, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import {Line} from 'react-chartjs-2';
 import Chart from 'chart.js';
 
 import Table from 'react-bootstrap/Table';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/esm/Col';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 class Charts extends Component{
 
@@ -98,28 +101,48 @@ class Charts extends Component{
         
         return (
             <div>
+            <Navbar bg="dark" variant="dark">
+                <Nav.Link >
+                <Link to = {'/Assets'} className = 'nav-link'>Back</Link>
+                </Nav.Link>
+            </Navbar>
                 <div>
                 {data.map ((item)=>{
-                    return(
-                    <Table className = 'table table-striped' bordered responsive bg = 'dark' fluid = 'md' variant = 'dark'>
-                        <Row bg = 'dark' >
-                            <Col>Project Id: {item.a_projectid}</Col>
-                            <Col>Meter Name: {item.a_metername}</Col>
-                        </Row>
-                        <Row bg = 'dark' variant = 'dark'>
-                            <Col>Department: {item.a_dept}</Col>
-                            <Col>Meter Description: {item.a_meterdesc}</Col>
-                            <Col>Units: {item.a_meterunits}</Col>
-                        </Row>
-                        <Row bg = 'dark'>
-                            <Col>Goal Group: {item.a_goalgroup}</Col>
-                        </Row>
-                    </Table>
+                         return(
+                            <body>
+                            <p>Project Id: {item.a_projectid}</p>
+                            <p>Meter Name: {item.a_metername}</p>
+                            <p>Department: {item.a_dept}</p>
+                            <p>Meter Description: {item.a_meterdesc}</p>
+                            <p>Meter Reading: {item.a_meterreading}</p>
+                            <p>Meter Units: {item.a_meterunits}</p>
+                            <p>Read Date: {item.a_readdate}</p>
+                            <p>Group: {item.a_group}</p>
+                            <p>Goal Group: {item.a_goalgroup}</p>
+                        </body>
+            
+                            // <Table className = 'table table-striped' bordered responsive bg = 'dark' fluid = 'md' variant = 'dark'>
+                            //     <Row bg = 'dark' >
+                            //         <Col>Project Id: {item.a_projectid}</Col>
+                            //         <Col>Meter Name: {item.a_metername}</Col>
+                            //     </Row>
+                            //     <Row bg = 'dark' variant = 'dark'>
+                            //         <Col>Department: {item.a_dept}</Col>
+                            //         <Col>Meter Description: {item.a_meterdesc}</Col>
+                            //         <Col>Units: {item.a_meterunits}</Col>
+                            //     </Row>
+                            //     <Row bg = 'dark'>
+                            //         <Col>Goal Group: {item.a_goalgroup}</Col>
+                            //     </Row>
+                            // </Table>
                  ) })}
                 </div> 
                 <div>
                     <Line data = {dataSet} />
                 </div>
+                <Navbar class = "navbar fixed-bottom" expand = 'lg' sticky = 'bottom' bg = 'dark'>
+                    <p></p>
+                </Navbar>
             </div>
             
         )
